5cabcacc0afac85f2eabd52265be065d50d4f662
ajezpeleta <ajezpeleta97@gmail.com>
2021-12-15 14:04:42 +0800

    Refactor scraper_bot.py

--- scraper_bot.py
+++ scraper_bot.py
@@ -1,27 +1,38 @@
 import csv
 import json
+import sys
 import time
+import logging
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
 from selenium.webdriver import Chrome
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def get_product_info(locs, store_id_, store_name_, prod_category, prod_info_list):
+class UnsupportedVendorError(Exception):
+    """
+        Thrown when an unsupported vendor id is given.
+
+        Contact the developer to request support for the vendor.
+    """
+    pass
+
+
+def get_product_info(in_vendor_id, in_vendor_name, in_category_name, in_on_page_selector, io_prod_info_list):
 
     while True:
-        # Locate the product elements on the page
-        # products_on_page = driver.find_elements(locs["type"], locs["productsOnPage"])
-        products_on_page = wait.until(ec.presence_of_all_elements_located((locs["type"], locs["productsOnPage"])))
+
+        products_on_page = wait.until(ec.presence_of_all_elements_located(
+                                        (in_on_page_selector["type"], in_on_page_selector["productsOnPage"])))
 
         # For each product in products_on_page, create a dictionary of product info
         for product in products_on_page:
             # Initialize product_info with default values
             product_info = {
-                "store": store_name_,
-                "category": prod_category,
+                "store": in_vendor_name,
+                "category": in_category_name,
                 "name": None,
                 "price": None,
                 "remarks": None,
@@ -31,11 +42,13 @@ def get_product_info(locs, store_id_, store_name_, prod_category, prod_info_list
             # product_info = dict(zip(key_list, value_list)
 
             # DynaQuestPC data parsing
-            if store_id_ == "dynaquestpc":
+            if in_vendor_id == "dynaquestpc":
 
-                product_info["name"] = product.find_element(locs["type"], locs["productName"]).text
+                product_info["name"] = product.find_element(in_on_page_selector["type"],
+                                                            in_on_page_selector["productName"]).text.replace(
+                    "\u2161", "II")
 
-                _price = product.find_element(locs["type"], locs["productPrice"]).text
+                _price = product.find_element(in_on_page_selector["type"], in_on_page_selector["productPrice"]).text
                 if _price.lower() == "sold out":
                     product_info["price"] = "N/A"
                     product_info["available"] = False
@@ -44,8 +57,10 @@ def get_product_info(locs, store_id_, store_name_, prod_category, prod_info_list
                     product_info["price"] = f'{float(_price):.2f}'
                     product_info["available"] = True
 
+                # PRODUCT REMARKS
+
             # EasyPC data parsing
-            elif store_id_ == "easypc":
+            elif in_vendor_id == "easypc":
 
                 # _json = json.loads(product.find_element(locs["type"], locs["productJSON"]).get_attribute("innerHTML"))
                 _json = json.loads(product.get_attribute("innerHTML"))
@@ -55,38 +70,41 @@ def get_product_info(locs, store_id_, store_name_, prod_category, prod_info_list
                 product_info["available"] = _json["available"]
 
             # PCHub data parsing
-            elif store_id_ == "pchub":
+            elif in_vendor_id == "pchub":
 
                 # Click product WebElement to open more info about the product
                 _prod = wait.until(ec.visibility_of(product))
                 driver.execute_script("arguments[0].click();", _prod)
 
-                product_info["name"] = product.find_element(locs["type"], locs["productName"]).text
+                product_info["name"] = product.find_element(in_on_page_selector["type"],
+                                                            in_on_page_selector["productName"]).text.replace(
+                    "\u2161", "II")
 
-                _price = product.find_element(locs["type"], locs["productPrice"]).text
+                _price = product.find_element(in_on_page_selector["type"], in_on_page_selector["productPrice"]).text
                 _price = _price.replace("\u20b1", "").replace(",", "").strip()
                 product_info["price"] = f'{float(_price):.2f}'
 
                 ok_status_list = ["in stock", "few remaining"]
-                _available = driver.find_element(locate_with(locs["type"],
-                                                             locs["productAvailable"]).below(product)).text
+                _available = driver.find_element(
+                    locate_with(in_on_page_selector["type"],
+                                in_on_page_selector["productAvailable"]).below(product)).text
                 if any(status in _available.lower() for status in ok_status_list):
                     product_info["available"] = True
                 else:
                     product_info["available"] = False
 
+            else:
+                raise UnsupportedVendorError(f'Unsupported vendor id "{in_vendor_id}"')
+
             # Append the current product_info dictionary to prod_info_list
-            prod_info_list.append(product_info)
+            io_prod_info_list.append(product_info)
 
         # Locate the next page button element and click it...
         # to continue scraping the remaining products in the current Category
         try:
-            next_page = driver.find_element(locs["type"], locs["nextPage"])
+            next_page = driver.find_element(in_on_page_selector["type"], in_on_page_selector["nextPage"])
             if next_page.get_attribute("class") != "disabled":
-                # next_page.click()
-                # time.sleep(0.5)
                 driver.execute_script("arguments[0].click();", next_page)
-                # driver.execute_script("window.scrollTo(0, 0)")
             else:
                 raise NoSuchElementException
         # If no such element was found, break away from the loop;
@@ -98,77 +116,147 @@ def get_product_info(locs, store_id_, store_name_, prod_category, prod_info_list
 
 def refresh_scroll_top(_driver):
     _driver.refresh()
-    _driver.execute_script("window.scrollTo(0, 0)")
+    _driver.execute_script("window.scrollTo(0, 0);")
 
 
 start_time = time.perf_counter()
 
-# Load the StoreWebsiteConfig.json config file
-with open("config-files/StoreWebsiteConfig.json", mode='r') as f:
-    store_website = json.load(f)
+# START --- Initialization of program variables from config file
+try:
+    with open("config-files/config.json", mode='r') as f:
+        config = json.load(f)
+        VENDOR_LIST = config["vendorList"]
+        # PRODUCT_INFO_HEADERS = config["productInfoHeaders"]
+        PRODUCT_INFO_HEADERS = ["store", "category", "name", "price", "remarks", "available"]
+        PRODUCT_INFO_LIST = []
+        TIMEOUT = config["timeout"] if isinstance(config["timeout"], (float, int)) else 10
+except FileNotFoundError or PermissionError as e:
+    # print("Error loading config file!")
+    print(e)
+    sys.exit()
 
-# Load the ProductInfoTemplate.json config file, then
-# Write the keys into a list
-with open("config-files/ProductInfoTemplate.json", mode='r') as f:
-    product_info_headers = json.load(f)
+# END --- Initialization of program variables from config file
 
-# product_info_list will contain the product_info dicts from get_product_info();
-# It will also be used to write to a CSV file
-product_info_list = []
+# START --- Vendor List loop
+for index, vendor in enumerate(VENDOR_LIST):
 
-# Iterate through the list of store websites
-for store_id, store_info in store_website.items():
-    store_name = store_info["name"]
-    urls = store_info["urls"]
-    product_category_list = urls.keys()
-    locators = store_info["locators"]
+    # START --- Assigning and validation of data from VENDOR_LIST
+    try:
+        vendor_id = vendor["id"]
+        vendor_name = vendor["name"]
+        baseURL = vendor["baseURL"]
+        product_category = vendor["productCategory"]
+        on_page_selector = vendor["onPageSelector"]
 
-    # if store_id in ["pchub"]:   # Test PCHub for now...
+        if not (all([vendor_id, vendor_name, baseURL, product_category, on_page_selector])):
+            raise ValueError(f'vendorList[{index}] has 1 or more missing required values')
+        elif type(vendor_id) is not str:
+            raise TypeError(f'Expected a type `string` for vendorList[{index}]["id"]')
+        elif type(vendor_name) is not str:
+            raise TypeError(f'Expected a type `string` for vendorList[{index}]["name"]')
+        elif type(baseURL) is not str:
+            raise TypeError(f'Expected a type `string` for vendorList[{index}]["baseURL"]')
+        elif type(product_category) is not dict:
+            raise TypeError(f'Expected a type `object` for vendorList[{index}]["productCategory"]')
+        elif type(on_page_selector) is not dict:
+            raise TypeError(f'Expected a type `object` for vendorList[{index}]["onPageSelector"]')
+    except KeyError as e:               # Skip current vendor if KeyError is caught
+        e.msg = f'vendorList[{index}] has a missing or misspelled key'
+        print(e.msg)
+        continue
+    except ValueError as e:             # Skip current vendor if ValueError is caught
+        print(e.msg)
+        continue
+    except TypeError as e:              # Skip current vendor if TypeError is caught
+        print(e.msg)
+        continue
+    # END --- Assigning and validation of data from VENDOR_LIST
+
+    # START --- WebDriver execution
     with Chrome() as driver:
-        wait = WebDriverWait(driver, 5)
-        home_url = urls["home"]
-        # driver.maximize_window()
-        driver.get(home_url)
-        # Add ExpectedCondition waiting for homepage to load
-        time.sleep(3)   # Using sleep() for now...
 
-        for product_category in product_category_list:
-            # Skip the home URL
-            # if product_category in ["home", "processor", "motherboard", "gpu", "ram", "psu"]:
-            if product_category == "home":
-                continue
+        wait = WebDriverWait(driver, TIMEOUT)     # Set explicit wait time for the WebDriver
 
-            # If no Category URL given, use the locator for WebDriver click() activity
-            if urls[product_category] is None:
-                if type(locators[product_category]) is list:
-                    for category_locator in locators[product_category]:
+        # START --- Loading the homepage of vendor; Connection testing
+        try:
+            driver.get(baseURL)
+        except TimeoutException as e:
+            e.msg = f'Timeout reached waiting for "{baseURL} to load'
+            print(e.msg)
+            continue
+        except WebDriverException as e:
+            e.msg = f'Cannot reach "{baseURL}" from id: {vendor_id}'
+            print(e.msg)
+            continue
+        # END --- Loading the homepage of vendor; Connection testing
+
+        # START --- Product Category loop
+        for category_name, category_info in product_category.items():
+
+            try:
+                if category_info["type"] == "url":
+                    if type(category_info["value"]) is str:
+                        driver.get(category_info["value"])
+                        get_product_info(vendor_id, vendor_name, category_name, on_page_selector, PRODUCT_INFO_LIST)
+                    elif type(category_info["value"]) is list:
+                        for _url in category_info["value"]:
+                            driver.get(_url)
+                            get_product_info(vendor_id, vendor_name, category_name, on_page_selector, PRODUCT_INFO_LIST)
+                    else:
+                        e_msg = f'Expected either type `string` or type `array` for ' \
+                                  f'vendorList[{index}]["productCategory"]["{category_name}"]["value"]'
+                        raise TypeError(e_msg)
+
+                elif category_info["type"] == "css selector" or "xpath":
+                    if type(category_info["value"]) is str:
                         refresh_scroll_top(driver)
-                        wait.until(ec.presence_of_element_located((locators["type"], category_locator))).click()
-                        get_product_info(locators, store_id, store_name, product_category, product_info_list)
-                else:
-                    refresh_scroll_top(driver)
-                    wait.until(ec.presence_of_element_located((locators["type"],
-                                                               locators[product_category]))).click()
-                    get_product_info(locators, store_id, store_name, product_category, product_info_list)
+                        wait.until(ec.presence_of_element_located(
+                            (category_info["type"], category_info["value"]))).click()
+                        get_product_info(vendor_id, vendor_name, category_name, on_page_selector, PRODUCT_INFO_LIST)
+                    elif type(category_info["value"]) is list:
+                        for _value in category_info["value"]:
+                            refresh_scroll_top(driver)
+                            wait.until(ec.presence_of_element_located((category_info["type"], _value))).click()
+                            get_product_info(vendor_id, vendor_name, category_name, on_page_selector, PRODUCT_INFO_LIST)
+                    else:
+                        e_msg = f'Expected either type `string` or type `array` for ' \
+                                  f'vendorList[{index}]["productCategory"]["{category_name}"]["value"]'
+                        raise TypeError(e_msg)
 
-            elif urls[product_category]:
-                # If a list of URLs for a Category was given, iterate through the list
-                if type(urls[product_category]) is list:
-                    for category_url in urls[product_category]:
-                        driver.get(category_url)
-                        get_product_info(locators, store_id, store_name, product_category, product_info_list)
-                # Else, just go to the given Category URL
                 else:
-                    driver.get(urls[product_category])
-                    get_product_info(locators, store_id, store_name, product_category, product_info_list)
-            else:
+                    e_msg = f'Expected a value of "url", "css selector", or "xpath" for ' \
+                              f'vendorList[{index}]["productCategory"]["{category_name}"]["type"]'
+                    raise ValueError(e_msg)
+
+            except TypeError as e:
+                print(e.msg)
+                continue
+            except ValueError as e:
+                print(e.msg)
+                continue
+            except TimeoutException as e:
+                e.msg = f'Timeout reached waiting for "{category_info["value"]} to load'
+                print(e.msg)
+                continue
+            except WebDriverException as e:
+                e.msg = f'Cannot reach "{category_info["value"]}" from id: {vendor_id}'
+                print(e.msg)
+                continue
+            except UnsupportedVendorError as e:
+                print(e)
                 break
+        # END --- Product Category loop
 
-# Write the extracted data to a CSV file
+    # END --- WebDriver execution
+
+# END --- Vendor List loop
+
+# START --- Writing of scraped data to output file
 with open("ProductInfo.csv", mode='w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=product_info_headers)
+    writer = csv.DictWriter(f, fieldnames=PRODUCT_INFO_HEADERS)
     writer.writeheader()
-    writer.writerows(product_info_list)
+    writer.writerows(PRODUCT_INFO_LIST)
+# END --- Writing of scraped data to output file
 
 end_time = time.perf_counter()
 print(f"Execution time: {(end_time - start_time)/60:0.4f} min")
